b7e0483c04da8b80427b1c524a19cfbabfdf2b1b
TiNredmc <thipok17@gmail.com>
2024-12-22 17:16:17 +0700

    Added launch file for maneuv3r.
    Signed-off-by: TiNredmc <thipok17@gmail.com>

---
+++ irob_launcher/launch/irob_maneuv3r.launch.py
@@ -0,0 +1,34 @@
+# iRob controller launcher 
+import os
+
+import launch
+import launch_ros.actions
+import launch_ros.descriptions
+
+from ament_index_python.packages import get_package_share_directory
+
+def generate_launch_description():
+
+    pkg_name = 'irob_maneuv3r'
+    
+    irob_maneuv3r_param_dir = launch.substitutions.LaunchConfiguration(
+        'irob_maneuv3r_param_dir',
+        default=os.path.join(
+            get_package_share_directory(pkg_name),
+            'params',
+            'irob_maneuv3r.yaml'))
+
+    irob_maneuv3r_instant = launch_ros.actions.Node(
+        package=pkg_name,
+        executable='iRob_maneuv3r',
+        output='screen',
+        parameters=[irob_maneuv3r_param_dir]
+    )
+    
+    return launch.LaunchDescription([
+        launch.actions.DeclareLaunchArgument(
+            'irob_maneuv3r_param_dir',
+            default_value=irob_maneuv3r_param_dir,
+            description='Path to iRob maneuv3r parameter yaml'),
+        irob_maneuv3r_instant,
+    ])
