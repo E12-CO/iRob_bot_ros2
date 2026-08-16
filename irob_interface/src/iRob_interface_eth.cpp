cec114d4d504b1c1811c4c3090586fbff1f11abb
Tin LetHax <thipok17@gmail.com>
2026-08-16 21:51:44 +0700

    iRob ETH interface node is almost fully working TODO: - More reliable disconnect detection. - Fix the hang on some node startup. - Implement the proper velocity command timeout to stop the motor.
    Signed-off-by: Tin LetHax <thipok17@gmail.com>

--- irob_interface/src/iRob_interface_eth.cpp
+++ irob_interface/src/iRob_interface_eth.cpp
@@ -25,7 +25,7 @@
 #include <irob_interface/msg_type.h>
 
 // Define Feedback Loop time 
-#define LOOP_TIME_MIL   20 // 20 millisec -> 50Hz
+#define LOOP_TIME_MIL   10 // 10 millisec -> 100Hz
 #define LOOP_TIME_SEC	LOOP_TIME_MIL/1000 // Loop time in second
 
 // Communication related
@@ -33,6 +33,8 @@
 
 #define ROS_COMM_TIMEOUT		5 	// consider communication timeout when 5 cycle elapsed (100ms)
 #define VEL_CMD_TIMEOUT			5 	// cmd vel timeout when 5 cycle elapsed (100ms)
+
+#define MAX_MOTOR_ID_CNT		8	// Maximum of motor ID grouping for iRob is 8 motors 
 
 // Conversion factor
 #define ENCODER_MAX_COUNT		65535.0f
@@ -44,6 +46,7 @@
 #define RAD_S_TO_RPM	9.549297	// 1 rad/s ~= 0.54 RPM
 #define SINE_120 		0.866025 	// Sine(120 degree) in rad unit
 #define F2_SQRT3      	1.1547	 	// 2/sqrt(3)
+
 
 class irob_eth_if : public rclcpp::Node{
 	
@@ -60,7 +63,9 @@ class irob_eth_if : public rclcpp::Node{
 		eIROB_STATE_QUERY		= 2,// iRob ETH parameter query state
 		eIROB_STATE_CONFIGURE	= 3,// iRob ETH parameter configuration state
 		eIROB_STATE_W8CONFIGURE = 4,// Wait iRob ETH parameter to configured
-		eIROB_STATE_RUN			= 5,// Active connection run state
+		
+		
+		eIROB_STATE_RUN			= 127,// Active connection run state
 	};
 	
 	// Structure to store specific parameter for each motor
@@ -76,19 +81,40 @@ class irob_eth_if : public rclcpp::Node{
 		float			f32SpeedMax;
 		uint32_t		u32EncoderCpr;
 		float			f32GearRatio;
+		int32_t			i32WheelId;
 	};
 
 	// Create a vector to store the parameter for each motor
 	std::vector<sMotorParameters> vectMotorParams;
+	
+	// Shared memory between ROS wall timer and TCP thead
+	std::array<std::atomic<float>, MAX_MOTOR_ID_CNT> velocity_cmd;
+	std::array<std::atomic<float>, MAX_MOTOR_ID_CNT> velocity_feedback;
+	int32_t i32MaxScannedWheelId;
+	
+	// Dedicated Joint state topics for iRob_controller
+	// Sensor publishers
+	rclcpp::Publisher<sensor_msgs::msg::JointState>::SharedPtr		pubIrobJointState;
+	sensor_msgs::msg::JointState	irobJointFeedback;
+	// Sensor subscriber
+	rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr	subIrobJointCommand;
+	
+	// Used in wall timer callback for publishing joint state feedback for irob controller
+	rclcpp::TimerBase::SharedPtr timerJointFb_;
+	
+	
 	
 	irob_eth_if() : Node("iRob_ETH_Interface"){
 		RCLCPP_INFO(
 			this->get_logger(), 
 			"Robot Club Engineering KMITL : Starting iRob ETH interface..."
 			);
-
+		
 		bool bIsMoreMotor = false;
 		uint32_t u32MotorIndex = 0;
+		
+		i32MaxScannedWheelId = -1;
+		
 		do{
 			// Declare and get the parameter of the motor
 			// in the format of "motor_N.motor_parameter"
@@ -111,12 +137,7 @@ class irob_eth_if : public rclcpp::Node{
 			
 			this->declare_parameter(sParamParent + "topic_name", rclcpp::PARAMETER_STRING);
 			if(this->get_parameter(sParamParent + "topic_name", sParam.sTopicName)){
-				RCLCPP_INFO(
-					this->get_logger(),
-					"Found motor_%d",
-					u32MotorIndex
-				);
-
+				
 				bIsMoreMotor = true;
 			}else{
 				// No or no more motor instant found, stop the search
@@ -154,8 +175,48 @@ class irob_eth_if : public rclcpp::Node{
 			this->declare_parameter(sParamParent + "gear_ratio", 1.0);
 			this->get_parameter(sParamParent + "gear_ratio", sParam.f32GearRatio);			
 
+			// Register wheel ID to be used for chassis controller
+			// Valid ID : 0, 1, 2, 3...
+			this->declare_parameter(sParamParent + "wheel_id", -1);
+			this->get_parameter(sParamParent + "wheel_id", sParam.i32WheelId);
+			
+			if(sParam.i32WheelId != -1){
+				RCLCPP_INFO(
+					this->get_logger(),
+					"Found motor_%d with iRob ID %d",
+					u32MotorIndex, sParam.i32WheelId
+				);
+			}else{
+				RCLCPP_INFO(
+					this->get_logger(),
+					"Found motor_%d",
+					u32MotorIndex
+				);
+			}
+			
+			if(sParam.i32WheelId > i32MaxScannedWheelId)
+				i32MaxScannedWheelId = sParam.i32WheelId;
+
 			// Push param into the array
 			vectMotorParams.push_back(sParam);
+			
+			// Look for duplicates in all array except self
+			for(int i=0; i < vectMotorParams.size() - 1; i++){
+				if(
+					(sParam.i32WheelId != -1) &&
+					(sParam.i32WheelId == vectMotorParams[i].i32WheelId)
+				){
+					RCLCPP_ERROR(
+						this->get_logger(),
+						"Error : found duplicate iRob motor ID %d at motor_%d!",
+						sParam.i32WheelId, u32MotorIndex
+					);
+					
+					std::raise(SIGTERM);
+					return;
+					
+				}
+			}
 			
 			// Increment to find next motor instant 	
 			u32MotorIndex++;			
@@ -174,6 +235,43 @@ exitMotorSearch:
 				this->get_logger(),
 				"Found total of %d motors in YAML parameter file",
 				u32MotorIndex
+			);
+		}
+		
+		if(
+			(i32MaxScannedWheelId > 2) && 
+			(i32MaxScannedWheelId < 9)
+		){
+			
+			RCLCPP_INFO(
+				this->get_logger(),
+				"Found %d wheel IDs! Creating joint state for iRob controller!",
+				i32MaxScannedWheelId + 1
+			);
+			
+			pubIrobJointState = create_publisher<sensor_msgs::msg::JointState>(
+				"irob_motor_feedback", 10
+			);
+			
+			irobJointFeedback.header.frame_id	= "quad_motor";
+			irobJointFeedback.velocity.resize(i32MaxScannedWheelId + 1);
+			
+			subIrobJointCommand = 
+				create_subscription<sensor_msgs::msg::JointState>(
+					"irob_motor_cmd",
+					10,
+					std::bind(
+						&irob_eth_if::irob_motor_cmd_callback,
+						this,
+						std::placeholders::_1)
+				);
+			
+			timerJointFb_ = this->create_wall_timer(
+				std::chrono::milliseconds(LOOP_TIME_MIL),
+				std::bind(
+					&irob_eth_if::irob_feedback_runner,
+					this
+					)
 			);
 		}
 		
@@ -197,6 +295,57 @@ exitMotorSearch:
 		RCLCPP_INFO(this->get_logger(), "iRob ETH interface started!");
 	}
 	
+	// Motor cmd callback from the topic "irob_motor_cmd" sent from iRob_controller node
+	void irob_motor_cmd_callback(const sensor_msgs::msg::JointState::SharedPtr motor_cmd){
+		if(
+			(motor_cmd->velocity.size() < 3) ||
+			(motor_cmd->velocity.size() > 4)
+		){
+			RCLCPP_ERROR(
+				this->get_logger(),
+				"Command error, the joint number %ld is incorrect!",
+				motor_cmd->velocity.size()
+			);
+			return;
+		}
+		
+		if(motor_cmd->header.frame_id != "quad_motor"){
+			RCLCPP_ERROR(
+				this->get_logger(),
+				"The joint command is not in the frame \"quad_motor\"!"
+			);
+			return;
+		}
+		
+		if(motor_cmd->velocity.size() > MAX_MOTOR_ID_CNT){
+			RCLCPP_ERROR(
+				this->get_logger(),
+				"The joint command size %ld exceed the motor count, command rejected!",
+				motor_cmd->velocity.size()
+			);
+			return;
+		}
+		
+		// Convert rad/s to RPM and send it to the controller 
+		for(size_t i=0; i <  motor_cmd->velocity.size(); i++)
+			velocity_cmd[0].store(motor_cmd->velocity[i] * RAD_S_TO_RPM);
+		
+				
+		cmd_vel_timeout_counter = 0;
+	}
+	
+	// Runner to send encoder velocity feedback to iRob_controller node via "irob_motor_feedback"
+	void irob_feedback_runner(void){
+		irobJointFeedback.header.stamp = this->get_clock()->now();
+		
+		for(size_t i = 0; i < i32MaxScannedWheelId; i++){
+		irobJointFeedback.velocity[i] = 
+			velocity_feedback[i].load(std::memory_order_relaxed);
+		}
+		
+		pubIrobJointState->publish(irobJointFeedback);
+	}
+	
 	// Return true if connected to iRob ETH server 
 	bool irob_isConnectedToIrob(int i32ConnectionFd){
 		struct sockaddr_in	sIRobConnectAddr;
@@ -211,6 +360,7 @@ exitMotorSearch:
 		
 	}
 	
+	// iRob_ETH protocol protocol packet helper function
 	void irob_protocolSetup(
 		tRosClientCommand *tCommandPacket,
 		uint8_t u8CmdType, 
@@ -225,6 +375,7 @@ exitMotorSearch:
 		tCommandPacket->u8DataLength	= u8CmdLen;
 	}
 	
+	// Check the validity of the returned packet from the iRob_ETH
 	bool irob_protocolReturnValidate(
 		tRosClientCommand *tCommandPacket,
 		tRosClientCommand *tReturnPacket
@@ -238,6 +389,7 @@ exitMotorSearch:
 			return false;
 	}
 	
+	// Write the protocol packet to socket
 	int irob_protocolWrite(int i32ConnectionFd, tRosClientCommand *tCommandPacket){
 		uint8_t u8Length;
 		
@@ -249,14 +401,15 @@ exitMotorSearch:
 		return write(i32ConnectionFd, (unsigned char*)&tCommandPacket->u8RbcHeader[0], u8Length);
 	}
 	
-	
+	// Thread that handling iRob_ETH connection, initialization, configuration and safety.
+	// Spawn by the number of motor configured in the YAML parameter 
 	void irob_tcpHandlerTask(sMotorParameters sParameterData){
 		// socket file descriptor
 		int i32iRobSocketFd;
 		
 		// received byte in socket buffer
-		int i32RxBytes;
-		bool bCanProcessRead;
+		int i32RxBytes = -1;
+		bool bCanProcessRead = false;
 		
 		// error return
 		int ret;
@@ -271,12 +424,15 @@ exitMotorSearch:
 		tRosClientCommand tRbcTxPacket;// Buffer for sending to the iRob ETH
 		tRosClientCommand tRbcRxPacket;// Buffer for receiving packet from iRob ETH
 		
+		rclcpp::Duration 	dControlSendDuration(0, 10000000U);// 0.01 sec -> 100Hz
+		rclcpp::Time   		tControlTick(0, 0);
 		// Holds the Joint command (motor speed)
 		float 	f32MotorJointCommand;
 		
 		// FSM 
 		unsigned int u32iRobEthFsm = eIROB_STATE_INIT;
 		unsigned int u32iRobConfigFsm = ePARAM_CTRL_KPIDFF;
+		
 		
 		RCLCPP_INFO(
 			this->get_logger(),
@@ -332,6 +488,8 @@ exitMotorSearch:
 				10,
 				lmbdJointCommandCallback
 			);	
+			
+		tControlTick = this->get_clock()->now();	
 		
 		while(1){
 			
@@ -378,7 +536,14 @@ exitMotorSearch:
 							*(float *)&tRbcRxPacket.u8InDataPtr[4] * RPM_TO_RAD_S;
 							
 						motorJointFeedback.header.stamp = this->get_clock()->now();
-							
+						
+						if(sParameterData.i32WheelId != -1){
+							velocity_feedback[sParameterData.i32WheelId].store(
+								motorJointFeedback.velocity[0],
+								std::memory_order_relaxed
+							);
+						}
+					
 						pubMotorJointState->publish(motorJointFeedback);
 						
 						bCanProcessRead = false;
@@ -468,7 +633,8 @@ exitMotorSearch:
 					if(irob_protocolReturnValidate(&tRbcTxPacket, &tRbcRxPacket)){
 						RCLCPP_INFO(
 							this->get_logger(),
-							"ParamStatus %d",
+							"[%s]ParamStatus %d",
+							sParameterData.sTopicName.c_str(),
 							((tParameterStatus *)&tRbcRxPacket.u8InDataPtr[0])->u8ParamStat
 						);
 						// 2. Parse the data bit to check the parameter is not configured
@@ -516,9 +682,11 @@ exitMotorSearch:
 					// - Control min, Control max
 					// - Encoder count per revolution (CPR)
 					// - Gear ratio
+					// - Control loop rate
+					// - Control loop enable
 					
 					switch(u32iRobConfigFsm){
-						case ePARAM_CTRL_KPIDFF:
+						case ePARAM_CTRL_KPIDFF:// Setup PID and feedforward gain
 						{	
 							// Copy data from ROS parameter struct to the TX packet 
 							
@@ -545,7 +713,7 @@ exitMotorSearch:
 						}
 						break;
 						
-						case ePARAM_CTRL_MINMAX:
+						case ePARAM_CTRL_MINMAX:// Setup control input min/max
 						{
 							// Copy data from ROS parameter struct to the TX packet 
 							
@@ -567,7 +735,7 @@ exitMotorSearch:
 						}
 						break;
 						
-						case ePARAM_CTRL_ENC_CPR:
+						case ePARAM_CTRL_ENC_CPR:// Setup encoder CPR
 						{
 							// Copy data from ROS parameter struct to the TX packet 
 							
@@ -587,7 +755,7 @@ exitMotorSearch:
 						}
 						break;
 						
-						case ePARAM_CTRL_GEAR_RATIO:
+						case ePARAM_CTRL_GEAR_RATIO:// Setup gear ration
 						{
 							// Copy data from ROS parameter struct to the TX packet 
 							
@@ -607,7 +775,7 @@ exitMotorSearch:
 						}
 						break;
 						
-						case eCONTROL_CTRL_LOOP_RATE:
+						case eCONTROL_CTRL_LOOP_RATE:// Setup control loop rate
 						{	
 							// Copy data from ROS parameter struct to the TX packet 
 							if(sParameterData.sLoopRate == "50HZ"){
@@ -631,7 +799,7 @@ exitMotorSearch:
 							}else{
 								RCLCPP_WARN(
 									this->get_logger(),
-									"[%s]Warning : Unknown loop rate : %s !",
+									"[%s]Warning : Unknown loop rate : %s ! Default to 50Hz",
 									sParameterData.sTopicName.c_str(),
 									sParameterData.sLoopRate.c_str()
 								);
@@ -653,7 +821,7 @@ exitMotorSearch:
 						}
 						break;
 						
-						case eCONTROL_CTRL_ON_OFF:
+						case eCONTROL_CTRL_ON_OFF:// Enable control loop
 						{
 							*(uint8_t *)(tRbcTxPacket.u8InDataPtr + 0x00) = 1;
 							
@@ -716,11 +884,13 @@ exitMotorSearch:
 					}else{
 						// Reset the config fsm back to start just in case for the next re-config
 						u32iRobConfigFsm = ePARAM_CTRL_KPIDFF;
-						u32iRobEthFsm = eIROB_STATE_RUN;
+						
+						// Jump back to wait connection state to rerun the parameter check
+						u32iRobEthFsm = eIROB_STATE_W8CONNECT;
 						
 						RCLCPP_INFO(
 							this->get_logger(),
-							"[%s]Configuration completed!",
+							"[%s]Configuration process completed! Rechecking the param status",
 							sParameterData.sTopicName.c_str()
 						);
 					}
@@ -728,10 +898,59 @@ exitMotorSearch:
 				}
 				break;
 				
-				case eIROB_STATE_RUN:
+				case eIROB_STATE_RUN:// Actively send the setpoint
 				{
-					
-					
+					if(
+						(this->get_clock()->now() - tControlTick)
+						> dControlSendDuration 
+					){
+						// Timestamping the tick
+						tControlTick = this->get_clock()->now();
+						
+						if(!irob_isConnectedToIrob(i32iRobSocketFd)){
+							RCLCPP_ERROR(
+								this->get_logger(),
+								"[%s]iRob ETH disconnected! Reconnecting...",
+								sParameterData.sTopicName.c_str()
+							);
+							u32iRobEthFsm = eIROB_STATE_INIT;
+							break;
+						}
+						
+						if(sParameterData.i32WheelId != -1)
+							f32MotorJointCommand = 
+								velocity_cmd[sParameterData.i32WheelId].load(std::memory_order_relaxed);
+						
+						// Copy the received joint command from topic 
+						*(float *)(&tRbcTxPacket.u8InDataPtr + 0x00) = f32MotorJointCommand;
+							
+						irob_protocolSetup(
+							&tRbcTxPacket,
+							eCOMMAND_CONTROL,
+							eCONTROL_CTRL_SETPOINT,
+							eRW_WRITE,
+							sizeof(float)
+						);
+						
+						// Send out the setpoint command
+						if(
+							ret = irob_protocolWrite(i32iRobSocketFd, &tRbcTxPacket),
+							(ret < 0)
+						){
+							if(errno != EAGAIN){
+								RCLCPP_ERROR(
+									this->get_logger(),
+									"[%s]Error protocol tx for eCONTROL_CTRL_SETPOINT with code %d",
+									sParameterData.sTopicName.c_str(),
+									errno
+								);
+								
+								// TODO : Find a better way to handle this error 
+							}
+						}
+						
+					}
+
 				}
 				break;
 				
