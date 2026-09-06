bc54d59fb3980bddd1c27670b83a38c4c1dbd8a4
Tin LetHax <thipok17@gmail.com>
2026-09-06 15:20:13 +0700

    Remapped the twist topic name to cmd_vel with namespace capability.
    Signed-off-by: Tin LetHax <thipok17@gmail.com>

--- irob_maneuv3r/src/iRob_maneuv3r.cpp
+++ irob_maneuv3r/src/iRob_maneuv3r.cpp
@@ -175,7 +175,7 @@ class irob_rbc_maneuv3r : public rclcpp::Node{
 		tf_listener_ =
 			std::make_shared<tf2_ros::TransformListener>(*tf_buffer_);
 		
-		pubMotion = create_publisher<geometry_msgs::msg::Twist>("cmd_vel_irob_auto", 10);
+		pubMotion = create_publisher<geometry_msgs::msg::Twist>("cmd_vel", 10);
 		
 		pubiRobStat = create_publisher<irob_msgs::msg::IrobCmdMsg>("irob_stat", 10);
 		
